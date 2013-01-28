23a7d4c1f7c63385ae13582e1415b353ba3f4ff4
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-01-28 00:58:33 -0500

    sql database

---
+++ lessons_v1.sql
@@ -0,0 +1,44 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.27-log - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             7.0.0.4319
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for phpacademy
+DROP DATABASE IF EXISTS `phpacademy`;
+CREATE DATABASE IF NOT EXISTS `phpacademy` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `phpacademy`;
+
+
+-- Dumping structure for table phpacademy.posts
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `contents` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table phpacademy.posts: ~10 rows (approximately)
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` (`id`, `title`, `contents`) VALUES
+	(1, 'This is the first post', 'Yes it is.'),
+	(2, 'The second post', 'Once again, yes it is.'),
+	(3, 'Third post yay!!', 'work already'),
+	(6, 'Wooooooooo', '<script type="text/javascript">alert("Hello");</script>'),
+	(7, 'Wooooooooo', '<sript type="text/javascript">alert("Hello");</script>'),
+	(8, 'Wooooooooo', '<sript type="text/javascript">alert("Hello");</script>'),
+	(9, 'Wooooooooo', '<sript type="text/javascript">alert("Hello");</script>'),
+	(10, 'Wooooooooo', '<sript type="text/javascript">alert("Hello");</script>'),
+	(11, 'test', '&lt;sript type=&quot;text/javascript&quot;&gt;alert(&quot;Hello&quot;);&lt;/script&gt;'),
+	(12, 'test', '&lt;sript type=&quot;text/javascript&quot;&gt;alert(&quot;Hello&quot;);&lt;/script&gt;');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
