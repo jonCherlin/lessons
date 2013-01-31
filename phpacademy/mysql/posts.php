42a43c0c536988a22e2d553c7f0358951a54ac71
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-01-30 23:24:56 -0500

    completed phpacademy part 5

--- phpacademy/mysql/posts.php
+++ phpacademy/mysql/posts.php
@@ -2,12 +2,20 @@
 
 include_once('resources/init.php');
 
-$query = mysql_query("SELECT `id`, `title`, `contents` FROM `posts`");
+$query = mysql_query("SELECT `title` FROM `posts` LIMIT 1,3");
+
+/*$query = mysql_query("SELECT `title` FROM `posts` ORDER BY `id` DESC LIMIT 2");*/
+
+/*$query = mysql_query("SELECT DISTINCT `title` FROM `posts`");*/
+
+/*$query = mysql_query("SELECT COUNT(1) FROM `posts`");
+$count = mysql_result($query, 0);
+
+echo $count;*/
 
 while ( $row = mysql_fetch_assoc($query) ) {
 	?>
-	<h2><a href="posts.php?id=<?php echo $row['id']; ?>"> <?php echo $row['title']; ?> </a></h2>
-	<p> <?php echo $row['contents']; ?></p>
+	<h2><a href="posts.php?id="> <?php echo $row['title']; ?> </a></h2>
 	<?php
 	
 
