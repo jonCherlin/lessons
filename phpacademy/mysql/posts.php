09c238b724f9c96306136074055369d53dfd659c
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-01-30 23:49:01 -0500

    completed phpacademy part 6

--- phpacademy/mysql/posts.php
+++ phpacademy/mysql/posts.php
@@ -2,7 +2,11 @@
 
 include_once('resources/init.php');
 
-$query = mysql_query("SELECT `title` FROM `posts` LIMIT 1,3");
+$query = mysql_query("SELECT `id`, `title`, `contents` FROM `posts` WHERE `id` = 3 OR `title` = 'This is the first post'");
+
+/*$query = mysql_query("SELECT `id`, `title`, `contents` FROM `posts` WHERE `id` = 3 AND `title` = 'Third post yay!!'");*/
+
+/*$query = mysql_query("SELECT `title` FROM `posts` LIMIT 1,3");*/
 
 /*$query = mysql_query("SELECT `title` FROM `posts` ORDER BY `id` DESC LIMIT 2");*/
 
@@ -13,12 +17,19 @@ $count = mysql_result($query, 0);
 
 echo $count;*/
 
-while ( $row = mysql_fetch_assoc($query) ) {
+/*while ( $row = mysql_fetch_assoc($query) ) {
 	?>
 	<h2><a href="posts.php?id="> <?php echo $row['title']; ?> </a></h2>
 	<?php
 	
 
+}*/
+
+while ( $row = mysql_fetch_assoc($query) ) {
+	?>
+	<h2><a href="post.php?id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?> </a></h2>
+	<p> <?php echo $row['contents']; ?> </p>
+	<?php
 }
 
 ?>
--- phpacademy/mysql/syntax.sql
+++ phpacademy/mysql/syntax.sql
@@ -4,4 +4,11 @@ INSERT INTO `posts` SET
 	`title` = 'The second post',
 	`contents` = 'Once again, yes it is.'*/
 	
-SELECT `id`, `title`, `contents` FROM `posts`
+/*SELECT `id`, `title`, `contents` FROM `posts`*/
+
+/*UPDATE `posts` SET
+	`title` = 'This is a post',
+	`contents` = 'New content wooo'
+WHERE `id` = 13*/
+
+DELETE FROM `posts` WHERE `id` = 13
