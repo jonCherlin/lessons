0e8fbfe3b31295557b58dbba47eb16ec74815bfc
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 00:44:09 -0500

    newboston 16 and 17

---
+++ new_boston/concatenation/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$day = 'Thursday';
+$date = 31;
+$year = 2013;
+
+//echo 'The date is Thursday 31 2013';
+
+//echo 'The date is <strong>'." $day "." $date "."$year</strong>";
+
+echo "The date is $day $date $year";
+
+?>
