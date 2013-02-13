01a8570a0c0de684d3c450b9e08ea27522a5995a
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 01:49:00 -0500

    new boston 22

--- new_boston/operators/index.php
+++ new_boston/operators/index.php
@@ -16,7 +16,7 @@ $text .= ' World';
 
 echo $text;*/
 
-$number1 = 10;
+/*$number1 = 10;
 $number2 = 9;
 
 if ($number1 > $number2) {
@@ -24,6 +24,22 @@ if ($number1 > $number2) {
 }
 else {
 	echo 'No.';
-}
+}*/
+
+//$sum = 10 + 20;
+//echo $sum;
+
+//$number1 = 100;
+//$number2 = 50;
+//$number3 = 2;
+
+//$result = ($number1 + $number2) / $number3;
+
+$number1 = 10;
+$number2 = 3;
+
+$result = $number1 % $number2;
+
+echo $result;
 
 ?>
