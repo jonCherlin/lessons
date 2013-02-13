e7e77cc6b39cee2649c99d40e234af1d647c01e9
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 01:30:21 -0500

    new boston part 21

--- new_boston/operators/index.php
+++ new_boston/operators/index.php
@@ -10,10 +10,20 @@
 
 echo $number1;*/
 
-$text = 'Hello';
+/*$text = 'Hello';
 
 $text .= ' World';
 
-echo $text;
+echo $text;*/
+
+$number1 = 10;
+$number2 = 9;
+
+if ($number1 > $number2) {
+	echo 'Yes.';
+}
+else {
+	echo 'No.';
+}
 
 ?>
