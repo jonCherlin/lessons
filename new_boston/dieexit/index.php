c9795ae8a5f5f31cb762485a7f872eebd9c2657f
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 03:12:26 -0500

    new boston part 29

---
+++ new_boston/dieexit/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*echo 'Hello ';
+
+die('ERROR. PAGE HAS ENDED.');
+
+echo ' World';*/
+
+mysql_connect('localhost', 'root', '') or die ('Could not connect to database');
+
+echo 'Connected!';
+
+?>
