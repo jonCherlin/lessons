9dad0688e51fd2d35f79fd21a90a7c3ddedf7e5b
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 02:35:44 -0500

    new boston 26

--- new_boston/loops/index.php
+++ new_boston/loops/index.php
@@ -1,10 +1,18 @@
 <?php
 
-$counter = 1;
+/*$counter = 1;
 
 while ($counter <= 10) {
 	echo $counter.' Hello<br>';
 	$counter++;
+}*/
+
+$counter = 1;
+
+do {
+	echo 'This will ALWAYS show once.<br>';
+	$counter++;
 }
+while($counter <= 10)
 
 ?>
