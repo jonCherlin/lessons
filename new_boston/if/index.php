38360bb0c4d79c306036208f23f6ad14f8d5cd52
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-02-13 01:04:42 -0500

    new_boston parts 18 and 19

---
+++ new_boston/if/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*$text = 'Something';
+
+if ($text == 'Something else') {
+	echo 'TRUE';
+} 
+else {
+	echo 'FALSE';
+}*/
+
+$number = 10;
+
+if ($number == 10) {
+	echo 'Equal to ten.';
+}
+else if ($number == 11) {
+	echo 'Equal to eleven.';
+}
+else {
+	echo 'Not equal';
+}
+
+
+?>
