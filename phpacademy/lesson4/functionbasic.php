f342d21614d3893fb725dc11e91cbc499af66f3a
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-03-31 17:01:02 -0400

    basic functions

---
+++ phpacademy/lesson4/functionbasic.php
@@ -0,0 +1,17 @@
+<?php
+
+/*echo "My name is ";
+myname();
+
+function myname()
+{
+	echo"Jonathan";
+}*/
+
+function yourname($name, $age) {
+	echo "Your name is ".$name." and you are ".$age." years old.";
+}
+
+yourname("Jonathan", "28");
+
+?>
