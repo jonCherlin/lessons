0d4e2446e777fcfff4838c9cdd05081af65572dd
Jonathan Cherlin <jonathan.cherlin@domandtom.com>
2013-03-31 23:34:48 -0400

    radio buttons

--- phpacademy/lesson4/checkboxes/index.php
+++ phpacademy/lesson4/checkboxes/index.php
@@ -1,10 +1,17 @@
-<form action="process.php" method="post">
+<!--checkboxes-->
+
+<!--<form action="process.php" method="post">
 	<p><input type="checkbox" name="agree"/> I agree with the Terms & Conditions</p>
 	<p><input type="submit" value="Register" /></p>
+</form>-->
+
+<!--radio-->
+
+<form action="process.php" method="post">
+	<p>
+		<input type="radio" name="language" value="PHP" />PHP<br />
+		<input type="radio" name="language" value="C" />C<br />
+		<input type="radio" name="language" value="BASIC" />BASIC<br />
+	</p>
+	<p><input type="submit" value="Submit"/></p>
 </form>
-
-<?php
-
-
-
-?>
--- phpacademy/lesson4/checkboxes/process.php
+++ phpacademy/lesson4/checkboxes/process.php
@@ -1,10 +1,19 @@
 <?php
 
-if(isset($_POST['agree']) && $_POST['agree'] == 'on') {
+//checkboxes
+
+/*if(isset($_POST['agree']) && $_POST['agree'] == 'on') {
 	echo 'Set, and correct value.';
 }
 else {
 	echo 'You must agree to the Terms & Conditions.';
+}*/
+
+//radio
+
+if(isset($_POST['language'])) {
+	$language = $_POST['language'];
+	echo 'You selected '.$language.'.';
 }
 
 ?>
